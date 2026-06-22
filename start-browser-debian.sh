444c26123a6a623a736d0ae9c97b47c754fbc250
Muhammad Awais <visioninfinity0007@gmail.com>
2026-06-22 17:55:24 +0500

    Use internal Chromium CDP port

--- start-browser-debian.sh
+++ start-browser-debian.sh
@@ -2,7 +2,7 @@
 set -e
 
 PROFILE_DIR="${CHROME_USER_DATA_DIR:-/data/chrome-profile}"
-DEBUG_PORT="${CDP_PORT:-9222}"
+DEBUG_PORT="${CHROME_DEBUG_PORT:-9333}"
 HOME_URL="${START_URL:-https://www.google.com}"
 
 mkdir -p "$PROFILE_DIR"
@@ -10,7 +10,7 @@ mkdir -p "$PROFILE_DIR"
 exec chromium \
   --no-sandbox \
   --disable-dev-shm-usage \
-  --remote-debugging-address=0.0.0.0 \
+  --remote-debugging-address=127.0.0.1 \
   --remote-debugging-port="$DEBUG_PORT" \
   --user-data-dir="$PROFILE_DIR" \
   --window-size=1366,768 \
